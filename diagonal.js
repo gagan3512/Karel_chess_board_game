483452032ff42655897bf8b0f906843cab8ddbf5
Gagan Tuli <gagan1825.cse18@chitkara.edu.in>
2021-03-01 20:49:10 +0530

    Create diagonal.js

---
+++ diagonal.js
@@ -0,0 +1,26 @@
+/**
+ * Welcome to the Stanford Karel IDE.
+ * This is a free space for you to 
+ * write any Karel program you want.
+ **/
+function main(){
+   //your code here
+   putBeeper();
+  my();
+  turnRight();
+  my();
+  turnRight();
+  my();
+  turnRight();
+  my();
+  
+ 
+}
+function my(){
+     move();
+  turnLeft();
+  move();
+  putBeeper();
+ }
+
+
